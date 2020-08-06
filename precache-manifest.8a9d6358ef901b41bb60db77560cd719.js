d34ceb8408963656a471351eb8bef18da5a1282a
Austin Keener <austingraphicandweb@gmail.com>
2020-08-05 20:19:33 -0400

    Updates

---
+++ precache-manifest.8a9d6358ef901b41bb60db77560cd719.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05683c0207047ece4c767852be2303ed",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "82ae99bbb3e0e22547f4",
+    "url": "/react-portfolio/static/js/2.11283df0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.11283df0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ace6096f714529ec4c39",
+    "url": "/react-portfolio/static/js/main.07cd8a6a.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  }
+]);
